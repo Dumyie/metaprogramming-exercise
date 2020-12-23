cb68ea1043904cf16d1277b8ce903d7d688eda6d
Dumisani Josh Nyumbeka <dsannie@live.com>
2020-12-23 16:46:38 +0200

    Initial code commit
    Outstanding work is read only properties

---
+++ main.py
@@ -0,0 +1,87 @@
+import types
+from dataclasses import dataclass
+from typing import Callable, Any
+
+
+class RecordMeta(type):
+    def __new__(cls, class_name, bases=None, dict=None):
+        new_attr = dict.copy()
+        for key, value in dict.items():
+            if not key.startswith("__"):
+                print("key " + str(key) + " value:" + str(value))
+                new_attr["__"+ key] = True
+                if value is None:
+                    raise TypeError("argument is not defined" + str(key))
+                elif key == '__str__':
+                    cls.show(cls, class_name, dict)
+                else:
+                    pass
+        
+
+        return super(RecordMeta, cls).__new__(cls, class_name, bases, new_attr)
+
+
+    def __repr__(self):
+        # prettify the printing of objects
+        val = str(self.__name__) + "(\n"
+        for k, v in self.__dict__.items():
+            if not k.startswith("_"):
+                val += "  # " + str(v.label) + "\n  "
+                is_string = False
+                for y, z in self.__annotations__.items():
+                    if y == k:
+                        if str(z) == "<class 'str'>":
+                            is_string = True
+                            break
+                if is_string:
+                    val += str(k) + "='{" + k + "}'\n"
+                else:
+                    val += str(k) + "={" + k + "}\n"
+                # print(k, getattr(self, k))
+
+        val += ")"
+        return val
+
+
+@dataclass
+class Field:
+    """
+    Defines a field with a label and preconditions
+    """
+    label: str
+    precondition: Callable[[Any], bool] = None
+
+
+class Record(metaclass=RecordMeta):
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            newdict = self.__annotations__
+            if len(newdict) < len(kwargs):
+                raise TypeError("Extra argument passed")
+            if len(newdict) > len(kwargs):
+                raise TypeError("missing argument")
+            if newdict[key] != type(value):
+                raise TypeError("Incorrect value type. Expected: "+ str(newdict[key]) + " got: " + str(type(value)))
+            precond = getattr(self, key).precondition
+            if precond is not None:
+                precond_status = getattr(self, key).precondition(value)
+                if not precond_status:
+                    raise TypeError("Precondition invalid")
+
+            setattr(self,key,value)
+            
+    
+    def __str__(self):
+        dict = self.__dict__
+        pretty_object = str(self.__class__)
+        return pretty_object.format(**dict)
+    
+
+class Person(Record):
+    """
+    A simple person record
+    """
+    name: str = Field(label="The name")
+    age: int = Field(label="The person's age", precondition=lambda x: 0 <= x <= 150)
+    income: float = Field(label="The person's income", precondition=lambda x: 0 <= x)
+
--- tests.py
+++ tests.py
@@ -4,6 +4,8 @@ from textwrap import dedent
 from dataclasses import dataclass
 from typing import Callable, Any, Dict
 
+from main import RecordMeta, Record
+
 
 @dataclass
 class Field:
@@ -13,32 +15,36 @@ class Field:
     label: str
     precondition: Callable[[Any], bool] = None
 
-# Record and supporting classes here
-
-class Record:
-    pass
 
 # Usage of Record
 class Person(Record):
     """
     A simple person record
-    """ 
-    name: str = Field(label="The name") 
+    """
+    name: str = Field(label="The name")
     age: int = Field(label="The person's age", precondition=lambda x: 0 <= x <= 150)
     income: float = Field(label="The person's income", precondition=lambda x: 0 <= x)
+
+    # def __init__(self, name, age, income):
+    #     self.name = name
+    #     self.age = age
+    #     self.income = income
+
 
 class Named(Record):
     """
     A base class for things with names
     """
-    name: str = Field(label="The name") 
+    name: str = Field(label="The name")
+
 
 class Animal(Named):
     """
     An animal
     """
-    habitat: str = Field(label="The habitat", precondition=lambda x: x in ["air", "land","water"])
+    habitat: str = Field(label="The habitat", precondition=lambda x: x in ["air", "land", "water"])
     weight: float = Field(label="The animals weight (kg)", precondition=lambda x: 0 <= x)
+
 
 class Dog(Animal):
     """
@@ -46,37 +52,36 @@ class Dog(Animal):
     """
     bark: str = Field(label="Sound of bark")
 
-# Tests 
+
+# Tests
 class RecordTests(TestCase):
     def test_creation(self):
         Person(name="JAMES", age=110, income=24000.0)
-        with self.assertRaises(TypeError): 
+        with self.assertRaises(TypeError):
             Person(name="JAMES", age=160, income=24000.0)
-        with self.assertRaises(TypeError): 
+        with self.assertRaises(TypeError):
             Person(name="JAMES")
-        with self.assertRaises(TypeError): 
+        with self.assertRaises(TypeError):
             Person(name="JAMES", age=-1, income=24000.0)
-        with self.assertRaises(TypeError): 
+        with self.assertRaises(TypeError):
             Person(name="JAMES", age="150", income=24000.0)
-        with self.assertRaises(TypeError): 
+        with self.assertRaises(TypeError):
             Person(name="JAMES", age="150", wealth=24000.0)
-    
+
     def test_properties(self):
         james = Person(name="JAMES", age=34, income=24000.0)
         self.assertEqual(james.age, 34)
         with self.assertRaises(AttributeError):
             james.age = 32
-    
+
     def test_str(self):
         james = Person(name="JAMES", age=34, income=24000.0)
         correct = dedent("""
         Person(
           # The name
           name='JAMES'
-
           # The person's age
           age=34
-
           # The person's income
           income=24000.0
         )
@@ -86,6 +91,9 @@ class RecordTests(TestCase):
     def test_dog(self):
         mike = Dog(name="mike", habitat="land", weight=50., bark="ARF")
         self.assertEqual(mike.weight, 50)
-        
+
+
+if __name__ == '__main__':
+    unittest.main()
 if __name__ == '__main__':
     unittest.main()
